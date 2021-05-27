eaa6c3297b22b7c464dd44a8b81455386067adb6
MarcinParda <marcin98parda@gmail.com>
2021-05-27 19:11:53 +0200

    create navbar component

---
+++ src/components/app/navbar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="nav ph-3 pv-1">
+            <button className="button__small button--primary">
+                Przywitaj się
+            </button>
+        </nav>
+    );
+}
+
+export default NavBar;

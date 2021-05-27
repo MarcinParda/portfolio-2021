796209ed3a4eefd85374424490338c9be778ee8f
MarcinParda <marcin98parda@gmail.com>
2021-05-27 22:36:26 +0200

    add fontawesome

--- src/components/app/navbar/NavBar.jsx
+++ src/components/app/navbar/NavBar.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const NavBar = () => {
     return (
         <nav className="nav ph-3 pv-1">
-            <button className="button__small button--primary">
-                Przywitaj się
+            <span className="span__button">
+                <i className="fa fa-github" />
+            </span>
+            <button className="button button__small button--primary">
+                Kontakt
             </button>
         </nav>
     );

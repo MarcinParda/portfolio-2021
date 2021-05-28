f177cf29bab878c1f16126ab7942e48cd05562dc
MarcinParda <marcin98parda@gmail.com>
2021-05-28 14:11:17 +0200

    add link to previous portfolio

--- src/components/app/WelcomeSection/WelcomeSection.jsx
+++ src/components/app/WelcomeSection/WelcomeSection.jsx
@@ -12,7 +12,8 @@ const WelcomeSection = () => {
             <article className="text-center">
                 <h1 className="mt-2">Junior React Developer</h1>
                 <div className="mt-2">I program things to make the world a simpler place.</div>
-                <div className="mt-1 text-bold">And this is my portfolio.</div>
+                <div className="mt-1 text-bold">And this is my portfolio. (WIP)</div>
+                <div className="mt-1 text-bold">My portfolio from 2020 is <a className="a" target="_blank" rel="noopener noreferrer" href={"https://marcinparda.github.io/portfolio-2020/"} >here</a>.</div>
             </article>
         </section>
     );

d1255ff7a1de96c1a8ad4e4090ea12daeb8677b9
MarcinParda <marcin98parda@gmail.com>
2021-05-28 19:40:13 +0200

    add changing languages

--- src/components/app/WelcomeSection/WelcomeSection.jsx
+++ src/components/app/WelcomeSection/WelcomeSection.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import face from '../../../img/face.png';
+import {useTranslation} from "react-i18next";
+import plFlag from "../../../img/plFlag.png";
+import ukFlag from "../../../img/enFlag.png";
 
 const WelcomeSection = () => {
+    const { t, i18n } = useTranslation();
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng).then(() => {});
+    };
 
     return (
         <section className="section mt-5">
-            <header className="text-center"><h2>Hi, I'm Marcin</h2></header>
+            <header className="text-center"><h2>{t("hi")}</h2></header>
             <div className="text-center mt-2">
                 <img className="img__face" alt="my face :D" src={face} />
             </div>
             <article className="text-center">
                 <h1 className="mt-2">Junior React Developer</h1>
-                <div className="mt-2">I program things to make the world a simpler place.</div>
-                <div className="mt-1 text-bold">And this is my portfolio. (WIP)</div>
+                <div className="mt-2">{t("I program")}.</div>
+                <div className="mt-1 text-bold">{t("And this is my portfolio")}. (WIP)</div>
                 <div className="mt-1 text-bold">My portfolio from 2020 is <a className="a" target="_blank" rel="noopener noreferrer" href={"https://marcinparda.github.io/portfolio-2020/"} >here</a>.</div>
+                <div className="mt-1 flex row-flex center-flex">
+                    <span
+                        className={`span__button span__button--icon-with-span ml-1`}
+                        onClick={() => changeLanguage('en')}
+                    >
+                        <img className="img__icon" alt="polish flag" src={ukFlag} />
+                        <span className={`${i18n.language === "en" ? "span__button--selected" : ""} pl-1`}>English</span>
+                    </span>
+                    <span className="pl-1">|</span>
+                    <span
+                        className={`span__button span__button--icon-with-span pl-1`}
+                        onClick={() => changeLanguage('pl')}
+                    >
+                        <img className="img__icon" alt="polish flag" src={plFlag} />
+                        <span className={`${i18n.language === "pl" ? "span__button--selected" : ""} pl-1`}>Polski</span>
+                    </span>
+                </div>
             </article>
         </section>
     );
---
+++ src/i18n.js
@@ -0,0 +1,42 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        resources: {
+            en: {
+                translation: {
+                    "hi": "Hi, I'm Marcin",
+                    "I program": "I program things to make the world a simpler place",
+                    "And this is my portfolio": "And this is my portfolio",
+                    "Allow me to introduce myself": "Allow me to introduce myself",
+                    "You can scroll through this section": "You can scroll through this section, if you are only here to see my projects",
+                    "Short story of my life": "Short story of my life",
+                    "Hi. My name is Marcin Parda.": "Hi. My name is Marcin Parda. I am 23 years old and a React developer with one year of commercial experience. From the teenage years I was interested in creating games and athletics.",
+                    "When I realized that running": "When I realized that running was not something I wanted to do professionally, I started training in the field of game design. By the end of high school, I got to know javascript and soaked up in this technology. At the beginning, I created browser games, then I became interested in designing websites.",
+                    "I started IT studies": "I started IT studies. In 2020 after classes with Ermlab Softwere, I got an offer to start internships in this company and this is how my adventure with commercial programming of web applications began."
+                },
+            },
+            pl: {
+                translation: {
+                    "hi": "Cześć, jestem Marcin",
+                    "I program": "Tworzę oprogramowanie, aby uczynić świat prostszym miejscem",
+                    "And this is my portfolio": "A to jest moje portfolio",
+                    "Allow me to introduce myself": "Pozwól, że się przedstawię",
+                    "You can scroll through this section": "Możesz przewinąć tę sekcję, jeśli jesteś tu, aby zobaczyć moje projekty",
+                    "Short story of my life": "Moja krótka historia",
+                    "Hi. My name is Marcin Parda.": "Cześć. Nazywam się Marcin Parda. Jestem 23-letnim React deweloperem z rokiem doświadczenia komercyjnego. Od nastoletnich lat interesowałem się tworzeniem gier oraz lekkoatletyką.",
+                    "When I realized that running": "Gdy zorientowałem się, że bieganie nie jest rzeczą, którą chcę zająć profesjonalnie zacząłem się dokształcać w dziedzinie game designu. Pod koniec liceum poznałem javascript i wsiąknąłem w tę technologię. Na początku tworzyłem gry przeglądarkowe, później zainteresowałem się projektowaniem stron internetowych.",
+                    "I started IT studies": "Zacząłem studia informatyczne. Po zajęciach z firmą Ermlab Softwere dostałem ofertę rozpoczęcia staży w tej firmie i tak rozpoczęła się moja przygoda z komercyjnym programowaniem aplikacji webowych."
+                },
+            },
+        },
+        lng: "en",
+        fallbackLng: "en",
+        defaultNS: "translation",
+    });
+
+export default i18n;

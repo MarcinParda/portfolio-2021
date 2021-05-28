ff3fb0ef19f7ca8eeb8e34ca6ba4fefa36dc8d55
MarcinParda <marcin98parda@gmail.com>
2021-05-28 02:03:30 +0200

    add AboutMe section

--- src/components/app/AboutMeSection/AboutMeSection.jsx
+++ src/components/app/AboutMeSection/AboutMeSection.jsx
@@ -3,11 +3,22 @@ import React from 'react'
 const AboutMeSection = () => {
 
     return (
-        <section className="section section--primary section--under-running-man pt-8">
-            <header className="text-center"><h1 className="text-white">Allow me to introduce myself.</h1></header>
-            <h5 className="mt-1 text-white text-center">(You can scroll through this section, if you are only here to see my projects.)</h5>
-            <article className="text-center">
-                <h1 className="mt-2">Junior React Developer</h1>
+        <section className="section section--primary section--under-running-man pt-8 pb-8">
+            <header className="text-center">
+                <h1 className="text-white">Allow me to introduce myself.</h1>
+                <h5 className="mt-1 text-white text-center">(You can scroll through this section, if you are only here to see my projects.)</h5>
+            </header>
+
+            <article>
+                <h3 className="mt-5">
+                    <div><h3 className="text-center text-white text-normal">Short story of my life</h3></div>
+                    <div><h3 className="text-center text-white mt-2">Hi. My name is Marcin Parda. I am 23 years old and a React developer with one year of commercial experience. From the teenage years I was interested in creating games.</h3></div>
+                    <div className="mt-2"><h3 className="text-center text-white">At the beginning, they were board games, because my home did not have access to the Internet, and thus no programming materials. The municipal library also did not have any items on programming.</h3></div>
+                    <div className="mt-2"><h3 className="text-center text-white">At the age of eleven, I started to play around changing graphics and sounds in games and see what happens. I liked it so much that it didn't take long until the first game development program appeared on my disk.</h3></div>
+                    <div className="mt-2"><h3 className="text-center text-white">I already knew then that I wanted to deal with programming or professional running in the future. Years passed and I honed myself in the art of creating games.</h3></div>
+                    <div className="mt-2"><h3 className="text-center text-white">At the end of high school I became interested in creating websites in javascript. I got so caught up in it that I abandoned my earlier plans to become a Frontend Developer. From that day on, I was able to complete my IT studies and find a satisfactory job, in which I have been working since July 2020.</h3></div>
+                    <div className="mt-2"><h3 className="text-center text-white">I have also recently been training in UI design. It is a process that I enjoy a lot. In my professional life, I strive to become a programmer and designer in my country that is recognizable and well remembered by my coworkers.</h3></div>
+                </h3>
             </article>
         </section>
     );
--- src/components/app/RunningMan/RunningMan.jsx
+++ src/components/app/RunningMan/RunningMan.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from 'react';
 import runningMan from '../../../img/running-man-drawing-31.gif';
 
 const RunningMan = ({revert, className}) => {
-    const [left, setLeft] = useState(0);
+    const [left, setLeft] = useState(revert ? window.innerWidth : -150);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setLeft(prev => prev > window.innerWidth ? -150 : prev + 1);
+            if(revert){
+                setLeft(prev => prev < -150 ? window.innerWidth : prev - 1);
+            } else {
+                setLeft(prev => prev > window.innerWidth ? -150 : prev + 1);
+            }
         }, 7.5);
         return () => clearInterval(interval);
     }, []);
 
     return (
         <div className={className}>
-            <img className="img__small" style={{left: `${left}px`, position: 'absolute', transform: `${revert ? "scaleY(-1)" : "scaleY(1)"}` }} alt="running man" src={runningMan} />
+            <img className="img__small" style={{left: `${left}px`, position: 'absolute', transform: `${revert ? "scale(-1)" : "scaleY(1)"}` }} alt="running man" src={runningMan} />
         </div>
     );
 }

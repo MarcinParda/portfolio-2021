e8175809d3d07058d8910a302419e1f0bec9d393
MarcinParda <marcin98parda@gmail.com>
2021-05-29 07:12:26 +0200

    test running man v3

--- src/components/app/RunningMan/RunningMan.jsx
+++ src/components/app/RunningMan/RunningMan.jsx
@@ -3,80 +3,24 @@ import runningMan from '../../../img/running-man-drawing-31.gif';
 
 const RunningMan = ({revert, className}) => {
     const [left, setLeft] = useState(revert ? window.innerWidth : -125);
-    const [top, setTop] = useState(revert ? -100 : 95);
-    const [rotate, setRotate] = useState(revert ? -100 : -90);
+    const [top, setTop] = useState(revert ? -80 : 80);
+    const [rotate, setRotate] = useState(revert ? -90 : -90);
 
     useEffect(() => {
         const interval = setInterval(() => {
             if (revert) {
                 setLeft(prev => {
-                    if (prev === -150) {
-                        setTop(-95);
+                    if (prev === -125) {
+                        setTop(-80);
                         setRotate(-90);
                     }
-                    if (prev < 0) {
-                        setTop(prevTop => {
-                            if (prevTop > -90) {
-                                return prevTop - .225;
-                            }
-                            return prevTop;
-                        });
-                        setRotate(prev => {
-                            if (prev < 90) {
-                                return prev + (90 / 425);
-                            }
-                            return prev;
-                        });
-                    }
-                    if (prev > window.innerWidth - 150) {
-                        setTop(prevTop => {
-                            if (prevTop < 0) {
-                                return prevTop + .25;
-                            }
-                            return prevTop;
-                        });
-                        setRotate(prevRotate => {
-                            if (prevRotate < 0) {
-                                return prevRotate + (90 / 425);
-                            }
-                            return prevRotate;
-                        });
-                    }
-                    return prev < -150 ? window.innerWidth : prev - 1;
+                    return prev < -125 ? window.innerWidth : prev - 1;
                 });
             } else {
                 setLeft(prev => {
                     if (prev === -125) {
-                        setTop(95);
+                        setTop(80);
                         setRotate(-90);
-                    }
-                    if (prev < 0) {
-                        setTop(prevTop => {
-                            if (prevTop > 0) {
-                                return prevTop - (112.5 / 125);
-                            }
-                            return prevTop;
-                        });
-                        setRotate(prev => {
-                            if (prev < 0) {
-                                return prev + (90 / 125);
-                            }
-                            return prev;
-                        });
-                    }
-                    if (prev > window.innerWidth - 125) {
-                        setTop(prevTop => {
-                            if (prevTop < 95) {
-                                return prevTop + .25;
-                            }
-                            return prevTop;
-                        });
-                        setRotate(prevRotate => {
-                            if (prevRotate < 90) {
-                                return prevRotate + (90 / 425);
-                            }
-                            return prevRotate;
-                        });
                     }
                     return prev > window.innerWidth ? -125 : prev + 1;
                 });
@@ -84,6 +28,68 @@ const RunningMan = ({revert, className}) => {
         }, 7.5);
         return () => clearInterval(interval);
     }, []);
+
+    useEffect(() => {
+        if (revert) {
+            if (left < 0) {
+                setTop(prevTop => {
+                    if (prevTop > -80) {
+                        return prevTop - (90 / 125);
+                    }
+                    return prevTop;
+                });
+                setRotate(prev => {
+                    if (prev < 90) {
+                        return prev + (90 / 425);
+                    }
+                    return prev;
+                });
+            }
+            if (left > window.innerWidth - 125) {
+                setTop(prevTop => {
+                    if (prevTop < 0) {
+                        return prevTop + .25;
+                    }
+                    return prevTop;
+                });
+                setRotate(prevRotate => {
+                    if (prevRotate < 0) {
+                        return prevRotate + (90 / 425);
+                    }
+                    return prevRotate;
+                });
+            }
+        } else {
+            if (left < 0) {
+                setTop(prevTop => {
+                    if (prevTop > 0) {
+                        return prevTop - (prevTop / -left);
+                    }
+                    return prevTop;
+                });
+                setRotate(prevRotate => {
+                    if (prevRotate < 0) {
+                        return prevRotate + (-prevRotate / -left);
+                    }
+                    return prevRotate;
+                });
+            }
+            if (left > window.innerWidth - 125) {
+                setTop(prevTop => {
+                    if (prevTop < 95) {
+                        return prevTop + .25;
+                    }
+                    return prevTop;
+                });
+                setRotate(prevRotate => {
+                    if (prevRotate < 90) {
+                        return prevRotate + (90 / 425);
+                    }
+                    return prevRotate;
+                });
+            }
+        }
+    }, [left]);
 
     return (
         <div className={className}>
